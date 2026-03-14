a0a3ea399d5d5fb3e3808109c64f8fddcf27083b
Tristan Walmsley <tristanwalmsley49@outlook.com>
2026-03-14 12:51:29 +0000

    Created all the page files.

--- login_page.py
+++ login_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈Team Enigmas FTW!!!!!!")
+st.title("this will be the login page")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
