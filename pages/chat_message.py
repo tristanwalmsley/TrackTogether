8574171905e5fe9c91ea868c68fbf0030bfcd7a6
T-P-S-CS <trpalaciosleeman@lancashire.ac.uk>
2026-03-15 12:46:02 +0000

    Commented chat page!

--- pages/chat_message.py
+++ pages/chat_message.py
@@ -17,9 +17,15 @@ st.set_page_config(
 
 show_header()
 
+# ---------------------------------------------------
+# Defines connections to databases.
+# ---------------------------------------------------
+
+# Chat connection.
 conn = sqlite3.connect("chat.db")
 cursor = conn.cursor()
 
+# Users connection.
 conn2 = sqlite3.connect("users.db")
 cursor2 = conn2.cursor()
 
@@ -28,11 +34,14 @@ st.Page("pages/chat_message.py")
 st.title("Chat:")
 user_id = st.session_state.get("user_id")
 
+# Makes user login in not logged in.
 if (user_id == None):
     st.switch_page("streamlit_app.py")
 
+# Gets module code from session storage.
 module_code = st.session_state.get("module_code")
 
+# Gets name from the database.
 cursor2.execute("SELECT fName FROM users WHERE id = ?", (user_id,))
 user = cursor2.fetchone()
 
@@ -42,14 +51,16 @@ else:
     st.error("User is not found!")
     st.stop()
 
+# Gets all of the records from the database.
 cursor.execute("SELECT fName, message FROM chat WHERE module_code = ? ORDER BY id", (module_code,))
 messages = cursor.fetchall()
 
+# Writes the name and message to the chat space.
 for name, msg in messages:
     with st.chat_message(name):
         st.write(msg)
 
-
+# Sets up the chat screen.
 prompt = st.chat_input("Say something")
 if prompt:
     cursor.execute(
@@ -57,6 +68,7 @@ if prompt:
         (username, prompt, module_code)
     )
 
+    # Commits the connection.
     conn.commit()
 
     with st.chat_message(username):
